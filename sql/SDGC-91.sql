022bc003a5043795d33db14eb123281706448b3d
Israel S <israelhelado@gmail.com>
2023-02-28 14:15:42 -0600

    SDGC-91
    Creación de la tabla medidaCCH en la base de datos

---
+++ sql/SDGC-91.sql
@@ -0,0 +1,23 @@
+#SDGC-91
+
+#Nueva tabla medida_cch
+
+-- sge.medida_cch definition
+
+CREATE TABLE `medida_cch` (
+  `id_medida_cch` int NOT NULL AUTO_INCREMENT,
+  `id_cliente` int NOT NULL,
+  `fecha` datetime NOT NULL,
+  `bandera_inv_ver` int NOT NULL,
+  `actent` int NOT NULL,
+  `metod` int NOT NULL,
+  `created_on` date DEFAULT NULL,
+  `created_by` varchar(50) DEFAULT NULL,
+  `updated_on` date DEFAULT NULL,
+  `updated_by` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_medida_cch`),
+  KEY `medida_cch_FK` (`id_cliente`),
+  CONSTRAINT `medida_cch_FK` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id_cliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+COMMIT;
