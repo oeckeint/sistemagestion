4bbd4e912e31164d10ee079f8426bac45093ccd4
Jesús Sánchez <oeckeint@gmail.com>
2023-11-18 13:34:46 -0600

    SDGC-125
    Cambiar valor de Importe Total Factura

--- src/main/java/datos/service/PeajesServiceImp.java
+++ src/main/java/datos/service/PeajesServiceImp.java
@@ -88,6 +88,7 @@ public class PeajesServiceImp implements datos.interfaces.DocumentoXmlService<Pe
         peaje.setCodFisFac(documento.getCodFisFac() + "-A");
         peaje.setTipFac("Av");
         peaje.setNumDias(documento.getNumDias() * -1);
+        peaje.setImpTotFac(peaje.getImpTotFac() * -1);
         
         peaje.setExcPot1(documento.getExcPot1() * -1);
         peaje.setExcPot2(documento.getExcPot2() * -1);
